63b90bb42b02d9dc8623a34a694c1dbfd68f5f6e
Pedro Piva <113521484+Pedro-Piva@users.noreply.github.com>
2025-12-11 11:58:05 -0300

    Update after_install.sh

--- codedeploy/after_install.sh
+++ codedeploy/after_install.sh
@@ -4,10 +4,8 @@ echo "=== AfterInstall: Configurando dependências ==="
 # 1. Define o diretório onde o appspec.yml colocou os arquivos
 APP_DIR="/home/ubuntu/app"
 
-# 2. Entra na pasta
 cd "$APP_DIR" || { echo "❌ Diretório $APP_DIR não encontrado"; exit 1; }
 
-# 3. Instala as dependências do projeto
 echo "Instalando pacotes npm..."
 npm install
 
